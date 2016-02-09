f0cf41222a3e82798a47873fba88b5c2be1ea797
ajeshmahto <ajesh.mahto@gmai.com>
2016-02-09 07:27:58 -0800

    Added- Bootstrpa, jquery. Welcome page as view, dependencies to rolve view, Configuration file for spring boot .

--- workManger/src/main/java/workForce/com/controller/UserController.java
+++ workManger/src/main/java/workForce/com/controller/UserController.java
@@ -1,8 +1,14 @@
 package workForce.com.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class UserController {
+	@RequestMapping("/welcome")
+    public String index(ModelAndView modelandview) {
+        return "/views/welcome";
+    }
 
 }
