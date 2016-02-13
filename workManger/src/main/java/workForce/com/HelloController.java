95599488511e6d336e3488d05609ec169723bc16
naraghisetareh <naraghi.setareh@gmail.com>
2016-02-13 00:56:57 -0800

    ...

--- workManger/src/main/java/workForce/com/HelloController.java
+++ workManger/src/main/java/workForce/com/HelloController.java
@@ -1,14 +1,26 @@
 package workForce.com;
 
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@ComponentScan
+@EnableAutoConfiguration
+@Configuration
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public String index(ModelAndView modelAndView) {
+    	
+    	System.out.println("test");
+    	
+        return "one";
     }
 
 }
